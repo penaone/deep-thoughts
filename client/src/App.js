e90e0831ade4f72a118a01c9e0a0131048f27ba8
penaone <penaone@gmail.com>
2020-12-30 10:56:13 -0700

    Thoughts Reactions and Friends pages completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // add these two library import statements
 import { ApolloProvider } from '@apollo/react-hooks';
@@ -6,6 +7,11 @@ import ApolloClient from 'apollo-boost';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const client = new ApolloClient({
   uri:'/graphql'
@@ -14,14 +20,24 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+    <Router>
       <div className="flex-column justify-flex-start min-100-vh">
         <Header />
         <div className="container">
-          <Home />
-        </div>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/signup" component={Signup} />
+    <Route exact path="/profile/:username?" component={Profile} />
+    <Route exact path="/thought/:id" component={SingleThought} />
+
+    <Route component={NoMatch} />
+  </Switch>
+</div>
         <Footer />
       </div>
-    </ApolloProvider>
+    </Router>
+  </ApolloProvider>
   );
 }
 
